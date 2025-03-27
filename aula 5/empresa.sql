8d1f3d36b783eb3f4f2897b98c6e7bb4df48fd59
Pedro Henrique Herrera <161768449+Herrera-Pedro@users.noreply.github.com>
2025-03-27 19:56:37 -0300

    Initial commit

---
+++ aula 5/empresa.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Empresa;
+
+USE Empresa;
+
+CREATE TABLE Funcionarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    cargo ENUM('Junior', 'Pleno', 'Senior') NOT NULL,
+    salario DECIMAL(10, 2) NOT NULL,
+    data_admissao DATE NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO Funcionarios 
+(nome, email, cargo, salario, data_admissao, ativo) 
+VALUES
+('Pedro', 'pedro.herrera@gmail.com', 'Senior', 9500.00, '2025-03-27'),
+('Matheus', 'matheus.galdino@gmail.com', 'Pleno', 6000.00, '2025-01-20')
+
+
